f5a59a96cce4c412182e3e61526e57ad514813aa
David Haukeness <david@hauken.us>
2023-08-07 16:56:39 -0600

    restrict daisy themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{svelte,ts}'],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: false,
   },
   plugins: [require("daisyui")],
 }
